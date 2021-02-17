f58558c7df2d56a0c42f4d06f345bf25790164b6
Sophie Huebler <shuebler@ou.edu>
2021-02-16 18:13:59 -0600

    Initial commit

---
+++ R/shinyRotate.R
@@ -0,0 +1,12 @@
+#' Interactive Data Cloud Rotation
+#'
+#' This function launches a shiny app to interact with a data cloud. Explore the effects
+#' of different rotations of the axes.You will be able to use the built in sample data
+#' set or one of your own. Choose which variables to plot and see how an axis rotation
+#' effects the covariance of the variables. Widgets are provided to allow you to change
+#' the visual asthetics of the data presentation.
+#'
+#' @examples
+shinyRotate<-function(){
+  shiny::runApp(system.file("Interactive", package="rotateS21"),launch.browser = TRUE)
+}
